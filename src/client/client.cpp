a2c88265ddad24b9f2bbdd92cc0645bd4e9c95bb
Arturas <arturas0000@gmail.com>
2022-11-16 18:13:48 +0200

    Added functionality to send multiple messages

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -17,17 +17,17 @@ void client_connect()
 {
     int sockfd, n;
 
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
+    struct sockaddr_in serv_addr{};
+    struct hostent *serverAddr;
+
+    serverAddr = gethostbyname(HOSTNAME);
 
     char buffer[256];
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) error("ERROR opening socket");
 
-    server = gethostbyname(HOSTNAME);
-
-    if (server == nullptr)
+    if (serverAddr == nullptr)
     {
         fprintf(stderr, "ERROR, no such host\n");
         exit(0);
@@ -35,26 +35,35 @@ void client_connect()
 
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr, server->h_length);
+    bcopy((char *) serverAddr->h_addr, (char *) &serv_addr.sin_addr.s_addr, serverAddr->h_length);
     serv_addr.sin_port = htons(PORT);
 
     if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
         error("ERROR connecting");
 
-    printf("Please enter the message: ");
-    bzero(buffer, 256);
-    fgets(buffer, 255, stdin);
-    n = write(sockfd, buffer, strlen(buffer));
+    int i = 0;
 
-    if (n < 0)
-        error("ERROR writing to socket");
+    while (true)
+    {
+        printf("Please enter the message: ");
+        bzero(buffer, 256);
+        fgets(buffer, 255, stdin);
+        n = write(sockfd, buffer, strlen(buffer));
 
-    bzero(buffer, 256);
-    n = read(sockfd, buffer, 255);
+        if (n < 0)
+            error("ERROR writing to socket");
 
-    if (n < 0)
-        error("ERROR reading from socket");
+        bzero(buffer, 256);
+        n = read(sockfd, buffer, 255);
 
-    printf("%s\n", buffer);
+        if (n < 0)
+            error("ERROR reading from socket");
+
+        printf("%s\n", buffer);
+
+        if (i == 10) break;
+        i += 1;
+    }
+
     close(sockfd);
 }
